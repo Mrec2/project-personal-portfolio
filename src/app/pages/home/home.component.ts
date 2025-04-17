786cbbdb4ae87d40e4740de01f6644c38989eba7
Mrec2 <marioteleco@outlook.com>
2025-04-17 19:12:12 +0200

    Pages and components with content and some styles were added. This is like first version called V1-Content

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ProjectsComponent, AboutComponent, SkillsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
